37f54aee2cd8720a6c274939d39bdbf22bc6734c
Thomas Mavrelis <tmavrelis@live.com>
2018-07-29 01:47:56 +0200

    remove spaces

--- books.c
+++ books.c
@@ -3,12 +3,8 @@
 #include<string.h>
 #define N 3
 
-
 int search(struct book,char*);
-
-
 void display(char**,int);
-
 
 struct info
 {
@@ -16,21 +12,18 @@ struct info
 	float price;
 };
 
-
 struct book
 {
 	char fname[15],lname[15],title[15];
 	struct info id; 
 };
 
-
 int main()
 {
 	struct book cat[N];
 	char pin[15];
 	int i,cnt=0;
-	
-	
+		
 	for(i=0;i<N;i++)
 	{
 		printf("Give info for %d book \n",i+1);
@@ -49,10 +42,8 @@ int main()
 		getchar();
 	}
 	
-	
 	printf("\n\nGive a title for search\n");
 	gets(pin);
-	
 
 	   if(!strcmp(cat[i].title,pin))
 	       cnt++;
